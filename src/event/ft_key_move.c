e5233e9307a67abe87e86fc34a164042bed9b0b1
Camy-Devlop <adbaibi.ismail@me.com>
2025-03-18 11:47:59 +0100

    ajout de la touche esc

--- src/event/ft_key_move.c
+++ src/event/ft_key_move.c
@@ -6,7 +6,7 @@
 /*   By: isadbaib <isadbaib@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 22:44:34 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/03/17 16:39:14 by isadbaib         ###   ########lyon.fr   */
+/*   Updated: 2025/03/18 11:45:13 by isadbaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ static bool ft_filt(int key)
     else if (key == 115)
         return (true);
     else if (65361 <= key && key <= 65364)
+        return (true);
+    else if (65307 <= key)
         return (true);
     else
         return (false);
@@ -46,6 +48,8 @@ int ft_key_move(int keycode, t_window data)
         ft_printf("valeur droit %d\n",keycode);
     else if (keycode == 65364)
         ft_printf("valeur bas %d\n",keycode);    
+    else if (keycode == 65307)
+        ft_close(data);    
     if (ft_filt(keycode))
         ft_printf("number of move %d\n",(data->cpt++));
     return (0);
