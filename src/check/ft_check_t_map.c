a6572c3d37918ef51a26c0fe52efb53b503dcc8f
Camy-Devlop <adbaibi.ismail@me.com>
2025-04-16 02:33:34 +0200

    fonction pour verfifier le mur

--- src/check/ft_check_t_map.c
+++ src/check/ft_check_t_map.c
@@ -3,14 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_check_t_map.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isadbaib <isadbaib@student.s19.be>         +#+  +:+       +#+        */
+/*   By: isadbaib <isadbaib@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:45:56 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/04/01 11:07:27 by isadbaib         ###   ########.fr       */
+/*   Updated: 2025/04/16 02:23:51 by isadbaib         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
+
+bool	ft_check_card_map(char **map, t_size_card *s)
+{
+	size_t i;
+	size_t j;
+	if (!map || !s)
+		return (false);
+	i = 0;
+	while (i < s->width)
+	{
+		if (map[0][i] != '1' || map[s->height-1][i] != '1')
+			return (false);
+	
+	}
+	return (true);
+}
 
 bool	ft_check_size_t_map(t_map m)
 {
