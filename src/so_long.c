a7c19b89e00bbf2e853f112644b5679e89418c50
Camy-Devlop <adbaibi.ismail@me.com>
2025-05-04 23:35:57 +0200

    so_long.c normer

--- src/so_long.c
+++ src/so_long.c
@@ -3,40 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isadbaib <isadbaib@student.s19.be>         +#+  +:+       +#+        */
+/*   By: isadbaib <isadbaib@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 15:23:51 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/04/26 20:47:51 by isadbaib         ###   ########.fr       */
+/*   Updated: 2025/05/03 19:19:28 by isadbaib         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+static bool	ft_init1(char **tab, t_game_room *room, t_map *m, t_card *tmp)
+{
+	*room = NULL;
+	*m = NULL;
+	*tmp = NULL;
+	(void)tmp;
+	if (!ft_file_check(tab[1]))
+		return (false);
+	*room = ft_calloc(1, sizeof(struct s_game_room));
+	if (!*room)
+		return (ft_printf("Error not create room"), false);
+	*m = ft_recover_card(tab[1]);
+	(*room)->card = ft_create_card(*m);
+	*m = NULL;
+	if (!(*room)->card)
+		return (free(*room), false);
+	(*room)->p = create_t_player(ft_cp_t_card((*room)->card));
+	if (!(*room)->p)
+		return (false);
+	*tmp = ft_cp_t_card((*room)->card);
+	if (ft_check_card_map((*tmp)->map, &(*room)->card->size) == false)
+	{
+		ft_free_t_card(*tmp, (*room)->card->size.height - 1);
+		*tmp = NULL;
+	}
+	return (true);
+}
+
+
+
 static t_game_room	ft_preparing_game(char **tab)
 {
-	t_game_room	room;
+  t_game_room	room;
 	t_map		m;
 	t_card		tmp;
 
-	if (!ft_file_check(tab[1]))
+	if (!ft_init1(tab, &room, &m, &tmp))
 		return (NULL);
-	room = ft_calloc(1, sizeof(struct s_game_room));
-	if (!room)
-		return (ft_printf("Error not create room"), NULL);
-	m = ft_recover_card(tab[1]);
-	room->card = ft_create_card(m);
-	m = NULL;
-	if (!room->card)
-		return (free(room), NULL);
-	room->p = create_t_player(ft_cp_t_card(room->card));
-	if (!room->p)
-		return (NULL);
-	tmp = ft_cp_t_card(room->card);
-	if (ft_check_card_map(tmp->map, &room->card->size) == false)
-	{
-		ft_free_t_card(tmp, room->card->size.height - 1);
-		tmp = NULL;
-	}
 	if (!tmp)
 		return (ft_free_room(room), NULL);
 	if (!back_traking(tmp, *room->p->coord, false))
@@ -52,6 +65,7 @@ static t_game_room	ft_preparing_game(char **tab)
 		return (ft_printf("Error collecter not find\n"), ft_free_t_card(tmp,
 				tmp->size.height - 1), ft_free_room(room), NULL);
 	return (ft_free_t_card(tmp, tmp->size.height - 1), room);
+
 }
 
 int	main(int n, char **tab)
