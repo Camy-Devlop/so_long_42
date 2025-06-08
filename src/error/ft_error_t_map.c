a366b7e7825cb8dd5c78ea0218d84fe729ee07b1
Camy-Devlop <adbaibi.ismail@me.com>
2025-06-08 21:57:21 +0200

    tout es fixe plus de bug

--- src/error/ft_error_t_map.c
+++ src/error/ft_error_t_map.c
@@ -6,7 +6,7 @@
 /*   By: isadbaib <isadbaib@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 14:53:02 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/03/07 15:35:08 by isadbaib         ###   ########.fr       */
+/*   Updated: 2025/06/08 21:54:20 by isadbaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_err_t_map(size_t err)
 	if (err == 0)
 		ft_printf("Error heigth ou width not correct \n");
 	if (err == 1)
-		ft_printf("Error the map must not be a square\n");
+		ft_printf("Error the map is very big\n");
 	if (err == 2)
 		ft_printf("Error not-regular rectangle\n");
 }
