007540af7b024fa8168d1536766d6b988f6edf9b
Camy-Devlop <adbaibi.ismail@me.com>
2025-06-15 16:48:02 +0200

    effacer les commentaire

--- src/player/ft_search_gamer.c
+++ src/player/ft_search_gamer.c
@@ -6,7 +6,7 @@
 /*   By: isadbaib <isadbaib@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 13:30:09 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/04/04 23:17:25 by isadbaib         ###   ########.fr       */
+/*   Updated: 2025/06/15 16:40:51 by isadbaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,61 +88,3 @@ t_player	create_t_player(t_card c)
 	ft_free_t_card(c, c->size.height - 1);
 	return (p);
 }
-// ft_err_t_card(1)
-/*
-void	ft_print_map(t_card m)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < m->size.height)
-		printf("%s",m->map[i++]);
-	printf("--------------\n");
-}
-int	main(void)
-{
-	char **m = calloc(20, sizeof(char *));
-	int i= 0;
- m[0] =strdup("1111111111111");
-
-
- m[1] =strdup("10010000000C1");
- m[2] =strdup("1000011111001");
- m[3] =strdup("1P0011E000001");
- m[4] =strdup("1111111111111");
- m[5] =strdup("10010100011110110001");
- m[6] =strdup("10100001000000001001");
- m[7] =strdup("1P100110001101100101");
- m[8] =strdup("10001000110000010001");
- m[9] =strdup("10110001001011001101");
- m[10]=strdup("10000100100100000001");
- m[11]=strdup("11001000010001011101");
- m[12]=strdup("10100011100110000001");
- m[13]=strdup("10001000001000100101");
- m[14]=strdup("10110100110011010001");
- m[15]=strdup("10000001000100001001");
- m[16]=strdup("10001100011001100101");
- m[17]=strdup("1E100010100010000001");
- m[18]=strdup("10110001001100011101");
- m[19]=strdup("11111111111111111111");
-
-	t_card c = calloc(1, sizeof(struct s_card));
-	c->map = m;
-	c->size.height = strlen(c->map[0]);
-	c->size.width = 5;
-	t_coord coord = {11, 3};
-	bool t = back_traking(c, coord, false);
-	if (t)
-	{
-		printf("trouver\n");
-	}
-	else
-	{
-		printf("non trouver\n");
-	}
-	while(i < 5)
-		free(c->map[i++]);
-	free(c);
-	free(m);
-	return (0);
-}*/
